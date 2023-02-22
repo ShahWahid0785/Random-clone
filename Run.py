8532960891252b26bf196f01bb221dd4ef1852aa
ShahWahid0785 <120056749+ShahWahid0785@users.noreply.github.com>
2023-02-22 16:10:44 +0430

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,24 @@
+port os, platform, time, sys
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+xoss('\n\x1b[1;37m[●] Checking Update........✔️✔️');time.sleep(0.5)
+os.system("git pull")
+def Update():
+    exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
+            xoss("\x1b[1;92m[●] Your Device 64 BIT 💥");time.sleep(1)
+            xoss("\x1b[1;92m[●] FOLLOW MY CHANNEL THANKS");time.sleep(1)
+            os.system("xdg-open https://T.me/DIVEL_TEAM_HACK")
+            import Akeng
+        if bit == '32bit':
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
+            xoss("\x1b[1;92m[●] Your Device 32 BIT 💥");time.sleep(1)
+            xoss("\x1b[1;92m[●] FOLLOW MY CHANNEL THANKS");time.sleep(1)
+            os.system("xdg-open https://T.me/DIVEL_TEAM_HACK")
+            import file32
