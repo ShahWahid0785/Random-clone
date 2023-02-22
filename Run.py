49bc5047fb4a6a610aea318a97bf359668636df9
ShahWahid0785 <120056749+ShahWahid0785@users.noreply.github.com>
2023-02-23 02:15:39 +0430

    Update Run.py

--- Run.py
+++ Run.py
@@ -14,14 +14,14 @@ def Run():
             xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
             xoss("\x1b[1;92m[●] Your Device 64 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] JOIN MY TELEGRAM CHANNEL");time.sleep(1)
-            os.system("xdg-open https://t.me/DIVEL_TEAM_HACK")
+            os.system("xdg-open https://t.me/DIVEL_TEAM_HACK/")
             import Wking64
 
         elif bit == '32bit':
             xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
             xoss("\x1b[1;92m[●] Your Device 32 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] JOIN MY TELEGRAM CHANNEL");time.sleep(1)
-            os.system("xdg-open https://t.me/DIVEL_TEAM_HACK")
+            os.system("xdg-open https://t.me/DIVEL_TEAM_HACK/")
             import Wking32
             print(50*"-")
         else:
