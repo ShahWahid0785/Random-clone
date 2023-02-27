187f69dba9e3cbc9d76d458444a9590baf493e47
ShahWahid0785 <120056749+ShahWahid0785@users.noreply.github.com>
2023-02-27 11:58:05 +0430

    Update Run.py

--- Run.py
+++ Run.py
@@ -4,7 +4,7 @@ def xoss(z):
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.02)
-xoss('\n\x1b[1;37m[●] Checking Update........✔️✔️');time.sleep(0.5)
+xoss('\n\x1b[1;37m[●] Checking Update......✔️✔️');time.sleep(0.5)
 os.system("git pull")
 def Update():
     exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
@@ -15,14 +15,14 @@ def Run():
             xoss("\x1b[1;92m[●] Your Device 64 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] JOIN MY TELEGRAM CHANNEL");time.sleep(1)
             os.system("xdg-open https://t.me/DIVEL_TEAM_HACK/")
-            import Wking64
+            import WKING64
 
         elif bit == '32bit':
             xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
             xoss("\x1b[1;92m[●] Your Device 32 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] JOIN MY TELEGRAM CHANNEL");time.sleep(1)
             os.system("xdg-open https://t.me/DIVEL_TEAM_HACK/")
-            import Wking32
+            import WKING32
             print(50*"-")
         else:
             exit('\033[1;31m[●] Connection & Network Error 🤕')
