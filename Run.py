4d645ce1076f6331e9f2426b33ab4b712e6d023d
ShahWahid0785 <120056749+ShahWahid0785@users.noreply.github.com>
2023-03-01 08:15:13 +0430

    Update Run.py

--- Run.py
+++ Run.py
@@ -15,14 +15,14 @@ def Run():
             xoss("\x1b[1;92m[●] Your Device 64 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] JOIN MY TELEGRAM CHANNEL");time.sleep(1)
             os.system("xdg-open https://t.me/DIVEL_TEAM_HACK/")
-            import WKING64
+            import Wking64
 
         elif bit == '32bit':
             xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
             xoss("\x1b[1;92m[●] Your Device 32 BIT 💥");time.sleep(1)
             xoss("\x1b[1;92m[●] JOIN MY TELEGRAM CHANNEL");time.sleep(1)
             os.system("xdg-open https://t.me/DIVEL_TEAM_HACK/")
-            import WKING32
+            import Wking32
             print(50*"-")
         else:
             exit('\033[1;31m[●] Connection & Network Error 🤕')
