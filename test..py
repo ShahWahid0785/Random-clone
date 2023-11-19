c6012be7c5942c4876d02d4b394aac918bd52a8d
ShahWahid0785 <120056749+ShahWahid0785@users.noreply.github.com>
2023-11-19 16:31:27 +0430

    Delete test..py

--- test..py
+++
@@ -1,5 +0,0 @@
-#'Dalvik/2.1.0 (Linux; U; Android {vers}; {verq} Build/{xidid}) [FBAV/{str(random.randint(111,555))}.1.0.0.0;FBBV/{str(random.randint(111111111,999999999))};FBRV/0;FBPN/{fbs};FBLC/en_US;FBBD/{verq};FBSV/{str(vers)};'+'FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]')
-
-
-
-'Dalvik/2.1.0 (Linux; U; Android {vers}; {verq} Build/{xidid}) [FBAN/{str(random.randint(111,555))}.1.0.0.0;FBAV/{str(random.randint(111111111,999999999))};FBRV/0;FBPN/{fbs};FBLC/en_US;FBBV/{verq};FBDV/{str(vers)};'+'FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1424};FB_FW/1]')
